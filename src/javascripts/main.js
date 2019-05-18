6035b2e80862040812e0f3d5ad6269308bc8d3c8
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-05-17 20:03:29 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,21 @@
+import categoriesData from './helpers/data/categories-data';
+import productsData from './helpers/data/products-data';
+import typesData from './helpers/data/types-data';
+import util from './helpers/util';
 import 'bootstrap';
 import '../styles/main.scss';
 
+const testPromises = () => new Promise((resolve, reject) => {
+  categoriesData.getCategories()
+    .then(response => typesData.getTypesForCategories(response.data.categories)
+      .then(categoriesWithTypes => productsData.getProductsForTypes(categoriesWithTypes)
+        .then((finalBuiltData) => {
+          util.printToDom('app', finalBuiltData);
+        })))
+    .catch(error => reject(error));
+});
+
 const init = () => {
-  console.error('Hello, testing');
+  testPromises();
 };
 init();
